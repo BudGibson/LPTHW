7a9a8e392b3582769b17d1e9877cd7ec20330002
Bud Gibson <fpgibson@MacBook-Pro-1770.local>
2019-02-20 18:40:32 -0500

    Exercises 1–3 complete

---
+++ exp3b.py
@@ -0,0 +1,25 @@
+print("I will now count my chickens:")
+
+# Adds 25 to 5 (30/6)
+print("Hens", 25.0 + 30 / 6)
+# Substracts 75 modulo 4 from 100
+print("Roosters", 100.0 - 25 * 3 % 4)
+
+print("Now I will count the eggs:")
+# Computes 5 % 2 subtracts 1/4 and adds 6
+print(3.0 + 2 + 1 - 5 + 4 % 2 - 1/4 + 6)
+
+print("Is it true that 3.0 + 2 < 5 - 7?")
+# Tests whether 5 is less than -2
+print(3.0 + 2 < 5 - 7)
+
+print("What is 3.0 + 2?", 3.0 + 2)
+print("What is 5.0 - 7?", 5.0 - 7)
+
+print("Oh, that's why it's False.")
+
+print("How about some more.")
+
+print("Is it greater?", 5.0 > -2)
+print("Is it greater or equal?", 5.0 >= -2)
+print("Is it less or equal?", 5.0 <= -2)
